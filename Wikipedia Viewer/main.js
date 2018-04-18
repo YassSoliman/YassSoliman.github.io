b4efc70403b2df74316e3261bea112651477c9d9
Yasser Soliman <yassersoliman_1@hotmail.com>
2018-04-18 18:54:39 -0400

    Changed the design and added WIP

--- Wikipedia Viewer/main.js
+++ Wikipedia Viewer/main.js
@@ -1,7 +1,11 @@
+
 $(document).ready(function(){
-    $("#search").keyup(function(){
+    $("#search").keypress(function(evt){
         var searchQuery = $("#search").val();
-        $.ajax({
+        $("#livesearch").empty();
+        $("#livesearch").css("border", "0px");
+        if(evt.key == 'Enter'){
+            $.ajax({
             url: 'https://en.wikipedia.org/w/api.php',
             dataType: "jsonp",
             data: {
@@ -21,7 +25,7 @@ $(document).ready(function(){
                 var i = data.query.pages;
                 for(var j in i){
                 $("#results").append(
-                '<a href="https://en.wikipedia.org/wiki/' + i[j].title + '" target="_blank">' + 
+                '<a href="https://en.wikipedia.org/wiki/' + i[j].title + ' target="_blank">' + 
                   '<article id="result">' + 
                    '<h2>' + i[j].title + '</h2>' +
                    '<p>' + i[j].extract + '</p>' + 
@@ -30,6 +34,42 @@ $(document).ready(function(){
                 );
               }
             }
-          });
+          }); //End of ajax
+        }
     });
+
+    /* // In progress, trying to make a suggestion search bar
+    // TODO: remove the suggestion box when user clicks away/presses search
+    $("#search").keyup(function(){
+        var searchQuery = $("#search").val();
+        if(searchQuery != ''){            
+            $.ajax({
+                url: 'https://en.wikipedia.org/w/api.php',
+                dataType: 'jsonp',
+                data: {
+                    action: 'opensearch',
+                    format: 'json',
+                    search: searchQuery,
+                    namespace: '0',
+                    limit: 5
+                },
+                success: function(data) {
+                    $("#livesearch").empty();
+                    var i = data[1];
+                    for(var j in i){
+                        $("#livesearch").append(
+                            '<a href="https://en.wikipedia.org/wiki/' + i[j] + '" target="_blank">' +
+                                i[j] +
+                            '</a><br>'
+                        );
+                    }
+                    $("#livesearch").css("border", "1px solid black");
+                }
+            });
+        } else {
+            $("#livesearch").empty();
+            $("#livesearch").css("border", "0px");
+        }
+        
+    });*/
 });
