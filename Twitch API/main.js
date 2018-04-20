6ddf2edc9e98b9e2459a2093d2924eac77da52de
Yasser Soliman <yassersoliman_1@hotmail.com>
2018-04-20 17:26:31 -0400

    Used the twitchtv api

---
+++ Twitch API/main.js
@@ -0,0 +1,55 @@
+var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+
+$(document).ready(function(){
+    getInfo();
+    $(".selector").click(function(){        
+        $(".selector").removeClass("active");
+        $(this).addClass("active");
+        var status = $(this).attr('id');
+        if(status == "all"){
+            $(".online, .offline").removeClass("hidden");
+        } else if (status == "online"){
+            $(".online").removeClass("hidden");
+            $(".offline").addClass("hidden");
+        } else {
+            $(".offline").removeClass("hidden");
+            $(".online").addClass("hidden");
+        }
+    });
+});
+
+function getInfo(){
+    streamers.forEach(function(streamer){
+        function makeURL(type, name) {
+            return 'https://wind-bow.gomix.me/twitch-api/' + type + '/' + name + '?callback=?';
+        };
+        $.getJSON(makeURL("streams", streamer), function(data) {
+            var game,
+                status;
+            if (data.stream === null) {
+              game = "Offline";
+              status = "offline";
+            } else if (data.stream === undefined) {
+              game = "Account Closed";
+              status = "offline";
+            } else {
+              game = data.stream.game;
+              status = "online";
+            };
+            $.getJSON(makeURL("channels", streamer), function(data) {
+              var logo = data.logo != null ? data.logo : "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F",
+                name = data.display_name != null ? data.display_name : channel,
+                description = status === "online" ? ': ' + data.status : "";
+                html = '<div class="row ' + 
+                status + '"><div class="col-xs-2 col-sm-1" id="logo"><img src="' + 
+                logo + '" class="logo"></div><div class="col-xs-10 col-sm-3" id="user"><a href="' + 
+                data.url + '" target="_blank">' + 
+                name + '</a></div><div class="col-xs-10 col-sm-8" id="status">'+ 
+                game + '<span class="hidden-xs">' + 
+                description + '</span></div></div>';
+              status === "online" ? $("#streamers").prepend(html) : $("#streamers").append(html);
+            });
+          });
+    });
+}
